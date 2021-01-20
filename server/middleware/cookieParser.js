5c5187879ef478c2bbe814c11a0763fb62df0873
Tim Anderson <tdanderson805@gmail.com>
2021-01-19 20:33:45 -0800

    Complete Parsing Cookie

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,23 @@
 const parseCookies = (req, res, next) => {
+  console.log(req.header('Cookie'));
+  let cookieHeader = req.header('Cookie');
+  if (!cookieHeader) {
+    req.cookies = {};
+    next();
+    return;
+  }
+
+  let cookieArray = cookieHeader.split(';');
+  let cookieObject = {};
+  for (let i = 0; i < cookieArray.length; i++) {
+    let tempArray = cookieArray[i].split('=');
+    cookieObject[tempArray[0].trim()] = tempArray[1].trim();
+  }
+  req.cookies = cookieObject;
+  next();
 };
 
 module.exports = parseCookies;
+
+
+
